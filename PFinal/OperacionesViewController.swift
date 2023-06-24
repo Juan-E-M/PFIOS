b4f4693ee691f9746c79b744e9831b696aa14672
JuniorMolina23 <junior.molina@tecsup.edu.pe>
2023-06-24 13:59:40 -0500

    navegacion lista

---
+++ PFinal/OperacionesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OperacionesViewController.swift
+//  PFinal
+//
+//  Created by Mac20 on 24/06/23.
+//
+
+import UIKit
+
+class OperacionesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
