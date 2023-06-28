93c5c9c07a702a9d6664ca4ce486e46297457b5d
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-06-28 14:57:44 -0700

    avance imagen combustible

--- PFinal/CombustibleViewController.swift
+++ PFinal/CombustibleViewController.swift
@@ -6,16 +6,59 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseStorage
+import FirebaseDatabase
 
-class CombustibleViewController: UIViewController {
-
+class CombustibleViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagenPicker.delegate = self
+        
+        
+        
+    }
+    var kmImageSelected: UIImage?
+    var imagenPicker = UIImagePickerController()
+    @IBOutlet weak var facturaTextField: UITextField!
+    @IBOutlet weak var montoTotalTextField: UITextField!
+    @IBOutlet weak var kmTextField: UITextField!
+    @IBOutlet weak var kmImagemButton: UIButton!
+    @IBOutlet weak var facturaImagenTextField: UIButton!
+    
+    
+    @IBAction func kmFotoTapped(_ sender: Any) {
+        imagenPicker.sourceType = .savedPhotosAlbum
+        imagenPicker.allowsEditing = false
+        present(imagenPicker, animated: true, completion: nil)
+    }
+    @IBAction func facturaFotoTapped(_ sender: Any) {
+        
     }
     
-
+    @IBAction func enviarTapped(_ sender: Any) {
+        let imagenesFolder = Storage.storage().reference().child("imagenes").child("combustible")
+        let imagenData =  kmImageSelected!.jpegData(compressionQuality: 0.5)
+        let cargarImagen = imagenesFolder.child("\(NSUUID().uuidString).jpg").putData(imagenData!, metadata: nil) { (metadata, error) in
+            if error != nil {
+                
+                print("Ocurrió un error al subir imagen: \(error)")
+            } else {
+                print("todo fue exitoso")
+            }
+        }
+            
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        kmImageSelected = image
+        imagenPicker.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
