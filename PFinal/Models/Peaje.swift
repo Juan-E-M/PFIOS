7c9b4b343799d59475c26e692063c13aef67fd1c
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-07-05 10:46:02 -0700

    saldo avance

---
+++ PFinal/Models/Peaje.swift
@@ -0,0 +1,16 @@
+//
+//  Peaje.swift
+//  PFinal
+//
+//  Created by Juan E. M. on 5/07/23.
+//
+
+import Foundation
+
+class Peaje{
+    var factura = ""
+    var urlfactura = ""
+    var monto = ""
+    var ubicacionlatitud = ""
+    var ubicacionlongitud = ""
+}
