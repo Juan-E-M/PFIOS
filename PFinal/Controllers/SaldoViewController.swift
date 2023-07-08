fb098abfaa770982bfc04fb80bb01cfc9d797e0a
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-07-08 09:21:16 -0700

    tabla dinámica finalizada

--- PFinal/Controllers/SaldoViewController.swift
+++ PFinal/Controllers/SaldoViewController.swift
@@ -25,6 +25,17 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
     var registrosCombustibles:[Combustible] = []
     var registrosPeajes:[Peaje] = []
     var registrosOtros:[Otro] = []
+    var submontoCombustible = 0.0
+    var submontoOtros = 0.0
+    var submontoPeajes = 0.0
+    @IBOutlet weak var saldoLabel: UILabel!
+    
+    func calcularSaldoTotal() {
+        let saldoTotal = submontoCombustible + submontoPeajes + submontoOtros
+        saldoLabel.text = "S/. \(saldoTotal)"
+    }
+
+    
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         var title = ""
@@ -90,28 +101,61 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 44))
         headerView.backgroundColor = .lightGray
 
-        let headerLabel = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.bounds.width - 30, height: 44))
-        headerLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
-        headerLabel.textColor = .white
-        headerLabel.backgroundColor = .clear
-        headerLabel.font = UIFont.systemFont(ofSize: 14) // Ajusta el tamaño de la fuente según tus preferencias
-        headerLabel.textAlignment = .left // Ajusta la alineación del texto según tus preferencias
-        headerView.addSubview(headerLabel)
+        let titleLabel = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.bounds.width - 100, height: 44))
+        titleLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
+        titleLabel.textColor = .white
+        titleLabel.backgroundColor = .clear
+        titleLabel.font = UIFont.systemFont(ofSize: 14)
+        titleLabel.textAlignment = .left
+        headerView.addSubview(titleLabel)
+
+        let additionalLabel = UILabel(frame: CGRect(x: tableView.bounds.width - 85, y: 0, width: 70, height: 44))
+        
+        var montoForEachSection = "0.00"
+        let space = " "
+        switch section {
+        case 0:
+            let monto = String(format: "%.2f", self.submontoCombustible)
+            montoForEachSection = "S/.\(String(repeating: space, count: (10-monto.count)))\(monto)"
+        case 1:
+            let monto = String(format: "%.2f", self.submontoPeajes)
+            montoForEachSection = "S/.\(String(repeating: space, count: (10-monto.count)))\(monto)"
+        case 2:
+            let monto = String(format: "%.2f", self.submontoOtros)
+            montoForEachSection = "S/.\(String(repeating: space, count: (10-monto.count)))\(monto)"
+        default:
+            let monto = "0.00"
+            montoForEachSection = "S/.\(String(repeating: space, count: (10-monto.count)))\(monto)"
+        }
+        additionalLabel.text = montoForEachSection
+        
+        additionalLabel.textColor = .white
+        additionalLabel.backgroundColor = .clear
+        additionalLabel.font = UIFont.systemFont(ofSize: 14)
+        additionalLabel.textAlignment = .right
+        headerView.addSubview(additionalLabel)
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(sectionHeaderTapped(_:)))
         headerView.addGestureRecognizer(tapGesture)
         headerView.tag = section
 
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            headerLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
-            headerLabel.topAnchor.constraint(equalTo: headerView.topAnchor),
-            headerLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15),
-            headerLabel.bottomAnchor.constraint(equalTo: headerView.bottomAnchor)
+            titleLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
+            titleLabel.topAnchor.constraint(equalTo: headerView.topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: additionalLabel.leadingAnchor, constant: -5),
+            titleLabel.bottomAnchor.constraint(equalTo: headerView.bottomAnchor)
+        ])
+
+        additionalLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            additionalLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15),
+            additionalLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor)
         ])
 
         return headerView
     }
+
     
     //Eliminación de registro
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -121,8 +165,6 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
             return .delete
     }
-    
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
             if editingStyle == .delete {
                 
@@ -155,7 +197,7 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     
     
-    //Eliminar registros
+    //Funciones para eliminación de registros
     func eliminarRegistroCombustible(_ comb: Combustible) {
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Combustible").child(comb.id).removeValue { (error, _) in
             if let error = error {
@@ -174,10 +216,15 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                     }
                     self.registrosCombustibles.removeAll { $0.id == comb.id }
                     self.tableView.reloadData()
+                    self.calcularSaldoTotal()
                 }
             }
         }
     }
+    func calcularSubmontoCombustible() {
+        submontoCombustible = registrosCombustibles.reduce(0.0) { $0 + (Double($1.monto) ?? 0.0) }
+    }
+    
     
     func eliminarRegistroPeajes(_ peaje: Peaje) {
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Peaje").child(peaje.id).removeValue { (error, _) in
@@ -192,10 +239,15 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                 }
                 self.registrosPeajes.removeAll { $0.id == peaje.id }
                 self.tableView.reloadData()
+                self.calcularSaldoTotal()
                 
             }
         }
     }
+    func calcularSubmontoPeaje() {
+        submontoPeajes = registrosPeajes.reduce(0.0) { $0 + (Double($1.monto) ?? 0.0) }
+    }
+    
     
     func eliminarRegistroOtros(_ otro: Otro) {
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Otros").child(otro.id).removeValue { (error, _) in
@@ -215,20 +267,28 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                     }
                     self.registrosOtros.removeAll { $0.id == otro.id }
                     self.tableView.reloadData()
+                    self.calcularSaldoTotal()
                 }
             }
         }
+    }
+    func calcularSubmontoOtros() {
+        submontoOtros = registrosOtros.reduce(0.0) { $0 + (Double($1.monto) ?? 0.0) }
     }
     
     
     
     
-
+    //Cargar la data
     override func viewWillAppear(_ animated: Bool) {
+        self.submontoOtros = 0.0
+        self.submontoPeajes = 0.0
+        self.submontoCombustible = 0.0
         
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Combustible").observe(.value) { (snapshot) in
             if let combustibleSnapshot = snapshot.value as? [String: Any], !combustibleSnapshot.isEmpty {
                 self.registrosCombustibles.removeAll()
+                var submontoTemp = 0.0
                 for (key, value) in combustibleSnapshot {
                     if let combustibleData = value as? [String: Any],
                        let factura = combustibleData["factura"] as? String,
@@ -245,11 +305,14 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                         comb.id = key
                         comb.monto = monto
                         
+                        submontoTemp += Double(monto) ?? 0.0
                         self.registrosCombustibles.append(comb)
                     }
                 }
                 
+                self.submontoCombustible = submontoTemp
                 self.tableView.reloadData()
+                self.calcularSaldoTotal()
             }
         }
         
@@ -257,6 +320,7 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Peaje").observe(.value) { (snapshot) in
             if let peajeSnapshot = snapshot.value as? [String: Any], !peajeSnapshot.isEmpty {
                 self.registrosPeajes.removeAll()
+                var submontoTemp = 0.0
                 for (key, value) in peajeSnapshot {
                     if let peajeData = value as? [String: Any],
                        let factura = peajeData["factura"] as? String,
@@ -275,11 +339,13 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                         peaje.destinolatitud = destinolatitud
                         peaje.destinolongitud = destinolongitud
                         
+                        submontoTemp += Double(monto) ?? 0.0
                         self.registrosPeajes.append(peaje)
                     }
                 }
-                
+                self.submontoPeajes = submontoTemp
                 self.tableView.reloadData()
+                self.calcularSaldoTotal()
             }
         }
         
@@ -288,6 +354,7 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
         Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("Otros").observe(.value) { (snapshot) in
             if let otroSnapshot = snapshot.value as? [String: Any], !otroSnapshot.isEmpty {
                 self.registrosOtros.removeAll()
+                var submontoTemp = 0.0
                 for (key, value) in otroSnapshot {
                     if let otroData = value as? [String: Any],
                        let tipodocumento = otroData["tipodocumento"] as? String,
@@ -303,11 +370,15 @@ class SaldoViewController: UIViewController, UITableViewDelegate, UITableViewDat
                         otro.id = key
                         otro.monto = monto
                         
+                        submontoTemp += Double(monto) ?? 0.0
                         self.registrosOtros.append(otro)
                     }
                 }
+                self.submontoOtros = submontoTemp
                 self.tableView.reloadData()
+                self.calcularSaldoTotal()
             }
         }
+        
     }
 }
